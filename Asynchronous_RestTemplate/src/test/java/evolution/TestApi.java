87c0e017fe6dd8ac887333dd7a705c7f512a14d1
chenli <chenli@192.168.43.24>
2017-06-13 17:23:32 +0800

    new demos

--- Asynchronous_RestTemplate/src/test/java/evolution/TestApi.java
+++ Asynchronous_RestTemplate/src/test/java/evolution/TestApi.java
@@ -9,12 +9,16 @@ public class TestApi {
 	@Test
 	public void run() {
 		AsynchronousRestTemplate template = new AsynchronousRestTemplate();
-		template.post("a", "http://localhost:8080/post", 
-				null, AnyDto.class);
+		for (int i = 0; i < 100; i++) {
+			template.post(i + "", "http://localhost:8080/post", 
+					null, AnyDto.class);
+		}
 		// This is an example of return type inference.
 		// You don't need to provide pass Class<T> as argument.
 		// The compiler can dynamically determine the return type.
-		AnyDto anyDto = template.get("a");
-		System.out.println(anyDto);
+		for (int i = 0; i < 100; i++) {
+			AnyDto anyDto = template.get(i + "");
+			System.out.println(anyDto);
+		}
 	}
 }
